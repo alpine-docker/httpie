9a513d6615afc7ee5254091419829b3bd75f6b15
Bill Wang <bill.wang>
2023-06-23 23:52:52 +1000

    feature/#6

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:3-alpine
 
 ARG VERSION
-RUN  python -m pip install --upgrade pip wheel httpie==${VERSION}
+RUN apk add build-base python3-dev && \
+  python -m pip install --upgrade pip wheel && \
+  python -m pip install httpie==${VERSION} && \
+  apk del build-base python3-dev
 
 ENTRYPOINT [ "http" ]
 CMD ["--help"]
--- build.sh
+++ build.sh
@@ -1,46 +1,55 @@
 #!/usr/bin/env bash
 
 # Prerequisite
-# Make sure you set secret enviroment variables in Travis CI
+# Make sure you set secret enviroment variables in CI
 # DOCKER_USERNAME
 # DOCKER_PASSWORD
-# API_TOKEN
 
-set -ex
+set -e
 
 Usage() {
   echo "$0 [rebuild]"
 }
 
+install_jq() {
+  # jq 1.6
+  DEBIAN_FRONTEND=noninteractive
+  #sudo apt-get update && sudo apt-get -q -y install jq
+  curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
+  sudo mv jq /usr/bin/jq
+  sudo chmod +x /usr/bin/jq
+}
+
+function get_latest_release() {
+  curl "$CURL_OPTIONS" "https://api.github.com/repos/$1/releases/latest" | jq -r '.tag_name | ltrimstr("v")'
+}
+
+build() {
+
+  if [[ ( "${CIRCLE_BRANCH}" == "master" ) ||( ${REBUILD} == "true" ) ]]; then
+
+      docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+      docker buildx create --use
+      docker buildx build --progress=plain --push \
+        --platform linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/amd64,linux/ppc64le,linux/s390x \
+        --build-arg VERSION=${tag} \
+        -t ${image}:${tag} \
+        -t ${image}:latest .
+  
+  fi
+}
+
 image="alpine/httpie"
-repo="jakubroztocil/httpie"
+repo="httpie/httpie"
 
-if [[ ${CI} == 'true' ]]; then
-  latest=`curl -sL -H "Authorization: token ${API_TOKEN}"  https://api.github.com/repos/${repo}/tags |jq -r ".[].name"|head -1`
-else
-  latest=`curl -sL https://api.github.com/repos/${repo}/tags |jq -r ".[].name"|head -1`
-fi
+install_jq
+tag=$(get_latest_release "${repo}")
+echo "Latest release is: ${tag}"
 
-sum=0
-echo "Lastest release is: ${latest}"
+status=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/${tag})
+echo $status
 
-tags=`curl -sL https://hub.docker.com/v2/repositories/${image}/tags/ |jq -r .results[].name`
-
-for tag in ${tags}
-do
-  if [ ${tag} == ${latest} ];then
-    sum=$((sum+1))
-  fi
-done
-
-if [[ ( $sum -ne 1 ) || ( $1 == "rebuild" ) ]];then
-  docker build --no-cache --build-arg VERSION=$latest -t ${image}:${latest} .
-  docker tag ${image}:${latest} ${image}:latest
-
-  if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker push ${image}:${latest}
-    docker push ${image}:latest
-  fi
-
+if [[ ( "${status}" =~ "not found" ) ||( ${REBUILD} == "true" ) ]]; then
+   echo "build image for ${tag}"
+   build
 fi
